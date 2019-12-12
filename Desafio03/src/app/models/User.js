7b1bfa89e5e07339d3fbbeb4ea144aa4c75ace91
Filipe de Oliveira Ribeiro <foribeiro@viceri.com.br>
2019-12-11 23:12:16 -0300

    inserido .env no arquivo gitignore

--- Desafio03/src/app/models/User.js
+++ Desafio03/src/app/models/User.js
@@ -10,7 +10,7 @@ class User extends Model {
         password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         permission_level: Sequelize.STRING,
-        provider: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
       },
       {
         sequelize,
--- Desafio03/src/config/database.js
+++ Desafio03/src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'gympoint',
+  host: process.env.POSTGRES_HOST,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
   define: {
     timestamps: true,
     underscored: true,
